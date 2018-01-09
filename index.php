a142373c203493443a8699901b0fad67c4f41877
Sstobo <seanstobo@gmail.com>
2018-01-09 06:36:07 -0800

    text input for front page

--- index.php
+++ index.php
@@ -16,16 +16,47 @@ get_header(); ?>
 						<div class="logo"></div>
 						<div class="menu"></div>
 					</div>
-					<div class="title"></div>
+					<div class="title">
+						<p> Nice to meet you, I'm </p>
+						<h1> Sean Stobo</h1>
+						<p> A full stack web and app developer based out of Vancouver, Canada. </p>
+
+
+					</div>
 				</div>
 
 				<div class="page2">
 					<div class="white-box">
 						<div class="pic"></div>
 						<div class="about-me">
-							<div class="about-me-intro"></div>
-							<div class="about-left"></div>
-							<div class="about-right"></div>
+							<div class="about-me-intro">
+								<h2> About Me </h2>
+								<p> I'm a dynamic and driven full stack front and back end developer. I'm a motivated and personable individual, who geniunly enjoys bringing projects to life with clean efficient code.
+									<h3> What im known for </h3>
+							</div>
+							<div class="about-left">
+								<p> Programming Proficiencys </p>
+								<ul> 
+									<li> HTML </li>
+									<li> CSS </li>
+									<li> Javascript </li>
+									<li> Jquery </li>
+									<li> React </li>
+									<li> Python </li>
+									<li> PHP </li>
+									<li> Sass/Gulp </li>
+								</ul>
+							</div>
+							<div class="about-right">
+								<p> What I do </p>
+								<ul> 
+									<li>Create immersive web experiences </li>
+									<li> App development for Android and iOS</li>
+									<li> Wordpress web development </li>
+									<li> Work collaborativly in a team environment </li>
+									<li> Provide a positive and energetic culture fit </li>
+								</ul>
+							</div>
 						</div>
 					</div>
 				</div>

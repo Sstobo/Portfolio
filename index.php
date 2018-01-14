426c02960b45158e00e64a0ecb954679f561ed65
Sstobo <32102061+Sstobo@users.noreply.github.com>
2018-01-14 13:57:32 -0800

    contact form styling

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * The main template file.
- *
+ * Tenplate Name: Main
  * @package RED_Starter_Theme
  */
 
@@ -70,38 +70,103 @@ get_header(); ?>
 				<div class="page3">
 					<div class="my-work">
 					<h2> My Work </h2>
-					<h4> Place Holder </h4>
-					
+					<h4> Nexus Intercambio</h4>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
+					<button class="view-site-button"> View Site </button></br>
+					<button class="prev-button"><- Prev</button></br>
+					<button class="next-button">Next  -></button>
 					</div>
 					<div class="my-work-pics"></div>
 				</div>
 
 				<div class="page4">
 					<div class="what-im-about">
+					
 						<div class="what-title">
-						
+						<h2> What Im About </h2>
 						</div>
-						<div class="what-left"></div>
-						<div class="what-right"></div>
+						<div class="what-left">
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+							</p>
+						</div>
+						<div class="what-right">
+							<p>
+							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+							</p>
+						</div>
 					</div>
 					<div class="what-cards">
-						<div class="card1"></div>
-						<div class="card2"></div>
-						<div class="card3"></div>
+						<div class="card1">
+						
+
+							<div class="card1-image">
+								<img class="image-white" src="<?php echo get_template_directory_uri(); ?>/images/mountain-white.svg">
+								<img class="image-black" src="<?php echo get_template_directory_uri(); ?>/images/mountain.svg">
+							</div>
+							<h4> Adventure </h4>
+							<p> 
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							</p>
+						</div>
+						<div class="card2">
+							
+							<div class="card2-image">
+								<img class="image-white" src="<?php echo get_template_directory_uri(); ?>/images/yoga-white.svg">
+								<img class="image-black" src="<?php echo get_template_directory_uri(); ?>/images/yoga.svg">
+							</div>
+							<h4> Adventure </h4>
+							<p> 
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							</p>
+						</div>
+						<div class="card3">
+							<div class="card3-image">
+							<img class="image-white" src="<?php echo get_template_directory_uri(); ?>/images/gi-white.svg">
+							<img class="image-black" src="<?php echo get_template_directory_uri(); ?>/images/gi.svg">
+							</div>
+							<h4> Adventure </h4>
+							<p> 
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+							</p>
+						</div>
 					</div>
 				</div>
 
 				<div class="page5">
 					<div class="education-wrapper">
-					<div class="education-left"></div>
-					<div class="education-right"></div>
+					<div class="education-left">
+<h2> My Education</h2>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat </p>
+<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat </p>
+
+					</div>
+					<div class="education-right">
+						<h3> Programs Completed and Achievements </h3>
+						<ul> 
+									<li><em>Full Stack Developer Professional </em>from Red Academy</li>
+									<li><em>Being a Leader</em> from Werner Erhardt </li>
+									<li><em>Leader Gifts</em> and <em>Leading Teams</em> from Earls Restaurants</li>
+									<li>Gracie Barra <em>Brazillian Jiu-Jitsu Instructors Certification</em> </li>
+									
+									<li><em>San Pelegrino Top Restaurant in Canada</em>, #60 Worldwide at Rouge Restaurant in Calgary </li>
+								</ul>
+								<h3> Volunteering </h3>
+						<ul> 
+									<li>Pemberton Brazillian Jiu-Jitsu <em>Kids Coach</em>  </li>
+									<li>Mt Curry Brazillian Jiu-Jitsu <em>Adult and Kids Coach</em> </li>
+									<li><em>Tutoring other Students</em> at Red Academy</li>
+									<li><em>Hiring Fair Co-ordinator</em> at Red Academy</li>
+								</ul>
+					</div>
 					</div>
 				</div>
 
 				<div class="page6">
 					<div class="contact-wrap">
 					<div class="contact-me"></div>
-					<div class="contact-form"></div>
+					<div class="contact-form">
+					<?php echo do_shortcode( '[contact-form-7 id="4" title="Contact form 1"]' ); ?>
+					</div>
 					</div>
 				</div>
 

5824cdf578ef78f3cfdb1b1bf9deeed4be1d828a
Sstobo <32102061+Sstobo@users.noreply.github.com>
2018-02-27 18:52:29 -0800

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'portfolio');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', '');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
